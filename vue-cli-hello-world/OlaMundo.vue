b1ba2a480a5ae856ab3e7642e3ec684171a3078b
luan11 <luaannovais@live.com>
2020-08-20 20:10:06 -0300

    Organizando diretorios e finalizando seçao 5

---
+++ vue-cli-hello-world/OlaMundo.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <input type="text" v-model="title">
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      title: 'Hello Vue'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
