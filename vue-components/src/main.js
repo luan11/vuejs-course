902d43e5068c06910fbbde959d0109061d8e6234
luan11 <luaannovais@live.com>
2020-08-21 15:15:08 -0300

    Aula 04 / Components

--- vue-components/src/main.js
+++ vue-components/src/main.js
@@ -1,12 +1,28 @@
 import Vue from 'vue'
 
+/* const data = {
+  title: 'VueJS Course',
+  count: 0
+}; */
+
 Vue.component('ln-component', {
   data: () => {
     return {
-      title: 'VueJS Course'
+      title: 'VueJS Course',
+      count: 0
+    };
+  },
+  methods: {
+    increment: function() {
+      this.count++;
     }
   },
-  template: '<h1>{{ title }}</h1>'
+  template: `
+  <div>
+    <h1>{{ title }}</h1>
+    <button @click="increment">Clicked {{ count }} times</button>
+  </div>
+  `
 })
 
 new Vue({
