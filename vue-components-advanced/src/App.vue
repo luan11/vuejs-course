076ccda4c4b269412a9cd0510089e8221ba4485d
luan11 <luaannovais@live.com>
2020-08-23 13:15:29 -0300

    Customizando o template com slots

---
+++ vue-components-advanced/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <h1>Vue</h1>
+
+    <!-- <Post :title="'Components'"/> -->
+    <Post>
+      <h2>Components Vue</h2>
+      <p>Lorem ipsum dolor sit amet consectetur.</p>
+    </Post>
+    <!-- <Post :title="'Data by props'"/> -->
+  </div>
+</template>
+
+<script>
+import Post from './components/Post'
+
+export default {
+  components: {
+    Post
+  }
+}
+</script>
