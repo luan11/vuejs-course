b23257753b07e5ef4a0b3d8d5a89c70b79ee260b
luan11 <luaannovais@live.com>
2020-09-28 15:53:03 -0300

    Detalhando navegação programatica

--- router/src/components/contacts/ContactsListItem.vue
+++ router/src/components/contacts/ContactsListItem.vue
@@ -1,6 +1,7 @@
 <template>
 	<li>
 		<span>{{ contact.name }}</span>
+		<!-- O MAIS CORRETO SEMPRE É TRABALHAR COM ROUTER LINKS -->
 		<router-link 
 			:to="{
 				name: 'contact',
@@ -12,6 +13,12 @@
 		>
 			Details
 		</router-link>
+		<!-- <button 
+			class="btn btn-info btn-sm float-right"
+			@click="viewDetails"
+		>
+			Details
+		</button> -->
 	</li>
 </template>
 
@@ -21,6 +28,28 @@ export default {
 		contact: {
 			type: Object,
 			required: true
+		}
+	},
+	methods: {
+		viewDetails() {
+			// this.$router.push(`/contacts/details/${this.contact.id}`)
+			/* this.$router.push({
+				path: `/contacts/details/${this.contact.id}`
+			}) */ /* Desta forma não é possivel trabalhar com parametros pois a rota não é nomeada */
+
+			/* this.$router.push({
+				name: 'contact',
+				params: {
+					id: this.contact.id
+				}
+			}) */
+			
+			this.$router.replace({
+				name: 'contact',
+				params: {
+					id: this.contact.id
+				}
+			})
 		}
 	}
 }
