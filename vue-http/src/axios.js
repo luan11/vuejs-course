262118d4e0de725fc854ad6a58ab1f88df75b0c5
luan11 <luaannovais@live.com>
2020-10-26 17:39:54 -0300

    Interceptando requisições

--- vue-http/src/axios.js
+++ vue-http/src/axios.js
@@ -5,4 +5,23 @@ const instance = axios.create();
 
 instance.defaults.baseURL = config.api.baseURL;
 
+/* instance.interceptors.request.use(config => {
+	console.log(config);
+
+	config.data = {
+		...config.data,
+		course: 'Vue JS'
+	};
+
+	return new Promise((resolve) => {
+		setTimeout(() => {
+			resolve(config);
+		}, 2000);
+	});
+}, error => {
+	console.log(error);
+
+	return error;
+}); */
+
 export default instance;
