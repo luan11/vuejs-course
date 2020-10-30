9982e5e9831b3b4004a8d973dd4e87f05325fa86
Luan Novais <luaannovais@live.com>
2020-10-30 15:32:10 -0300

    Trabalhando com requisiçoes simultaneas

--- vue-http/src/App.vue
+++ vue-http/src/App.vue
@@ -14,11 +14,36 @@
 </template>
 
 <script>
+
+import axios from 'axios'
+import config from './config/config'
+
+axios.defaults.baseURL = config.api.baseURL
+
 import TasksList from './components/TasksList'
 
 export default {
   components: {
     TasksList
+  },
+  created() {
+    /* axios.all([
+      axios.get('/tasks/1'),
+      axios.get('/tasks/3')
+    ])
+    .then(axios.spread((task1, task3) => {
+      console.log(task1, task3)
+    })); */
+
+    axios.all([
+      axios.get('/tasks/1'),
+      axios.get('/tasks/3')
+    ])
+    .then(response => {
+      const [task1, task3] = response;
+
+      console.log(task1, task3)
+    });
   }
 }
 </script>
