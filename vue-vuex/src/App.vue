133d8fdbd830eb32a917776342676e2fe0cad551
Luan Novais <luaannovais@live.com>
2020-11-03 19:37:07 -0300

    Iniciando seção sobre vuex

---
+++ vue-vuex/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="app">
+      <div class="jumbotron jumbotron-fluid">
+        <div class="container">
+          <h1 class="display-4">Vuex</h1>
+        </div>
+      </div>
+
+      <div class="container">
+        
+      </div>
+  </div>
+</template>
