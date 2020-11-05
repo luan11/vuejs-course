462961f3bdec31408741c257b4e69cd8c4efce96
Luan Novais <luaannovais@live.com>
2020-11-05 20:59:26 -0300

    Utilizando a função mapState

--- vue-vuex/src/components/Counter.vue
+++ vue-vuex/src/components/Counter.vue
@@ -1,6 +1,8 @@
 <template>
 	<div>
-		<h2 class="font-weight-light">Counter: {{ $store.state.counter }}</h2>
+		<h2 class="font-weight-light">Counter: {{ counter }}</h2>
+		<h4 class="font-weight-light">Counter Alias: {{ counterAlias }}</h4>
+		<h4 class="font-weight-light">Multiplicated Counter: {{ multiplicatedCounter }}</h4>
 
 		<button 
 			class="btn btn-lg btn-danger mr-2"
@@ -19,7 +21,25 @@
 </template>
 
 <script>
+import {mapState} from 'vuex'
+
 export default {
+	data() {
+		return {
+			localCounter: 2
+		}
+	},
+	/* computed: mapState([
+		'counter'
+	]), */
+	computed: mapState({
+		// counter: state => state.counter
+		counter: 'counter',
+		counterAlias: 'counter',
+		multiplicatedCounter(state) {
+			return state.counter * this.localCounter
+		}	
+	}),
 	methods: {
 		decrement() {
 			this.$store.state.counter--;
