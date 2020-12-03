ca9b708087199888bd12b8f59cd0f90a6af2e57c
Luan Novais <luaannovais@live.com>
2020-12-03 08:51:26 -0300

    Mutations do modulo tarefas

--- vue-vuex/src/resources/tasks/_store/mutation-types.js
+++ vue-vuex/src/resources/tasks/_store/mutation-types.js
@@ -1,1 +1,4 @@
+export const CREATE_TASK = 'CREATE_TASK';
+export const EDIT_TASK = 'EDIT_TASK';
+export const DELETE_TASK = 'DELETE_TASK';
 export const LIST_TASKS = 'LIST_TASKS';
--- vue-vuex/src/resources/tasks/_store/mutations.js
+++ vue-vuex/src/resources/tasks/_store/mutations.js
@@ -1,6 +1,19 @@
-import { LIST_TASKS } from './mutation-types'
+import { CREATE_TASK, EDIT_TASK, DELETE_TASK, LIST_TASKS } from './mutation-types'
 
 export default {
+	[CREATE_TASK]: (state, { task }) => {
+		state.tasks.push(task);
+	},
+	[EDIT_TASK]: (state, { task }) => {
+		const index = state.tasks.findIndex(t => t.id === task.id);
+
+		state.tasks.splice(index, 1, task);
+	},
+	[DELETE_TASK]: (state, { task }) => {
+		const index = state.tasks.findIndex(t => t.id === task.id);
+
+		state.tasks.splice(index, 1);
+	},
 	[LIST_TASKS]: (state, { tasks }) => {
 		state.tasks = tasks;
 	}
